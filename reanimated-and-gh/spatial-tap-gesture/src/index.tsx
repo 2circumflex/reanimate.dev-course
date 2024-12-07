21e693191344ab92ec9aaa69c638ba7761b6e280
Roy <2circumflex@gmail.com>
2024-12-07 09:41:30 +0900

    Spatial Tap Gesture

---
+++ reanimated-and-gh/spatial-tap-gesture/src/index.tsx
@@ -0,0 +1,115 @@
+import { StyleSheet } from 'react-native';
+import { StatusBar } from 'expo-status-bar';
+import { Gesture, GestureDetector } from 'react-native-gesture-handler';
+import Animated, {
+  cancelAnimation,
+  Easing,
+  useAnimatedReaction,
+  useAnimatedStyle,
+  useDerivedValue,
+  useSharedValue,
+  withSpring,
+  withTiming,
+} from 'react-native-reanimated';
+
+const CircleRadius = 30;
+
+const App = () => {
+  const left = useSharedValue(0);
+  const top = useSharedValue(0);
+  const scale = useSharedValue(0);
+
+  const prevLeft = useSharedValue(0);
+  const prevTop = useSharedValue(0);
+
+  const tapGesture = Gesture.Tap().onBegin(event => {
+    prevLeft.value = left.value;
+    prevTop.value = top.value;
+    left.value = event.x - CircleRadius;
+    top.value = event.y - CircleRadius;
+  });
+
+  const rStyle = useAnimatedStyle(() => {
+    return {
+      left: left.value,
+      top: top.value,
+      transform: [{ scale: scale.value }],
+    };
+  }, []);
+
+  const rPrevStyle = useAnimatedStyle(() => {
+    return {
+      left: prevLeft.value,
+      top: prevTop.value,
+    };
+  }, []);
+
+  useAnimatedReaction(
+    () => {
+      return left.value;
+    },
+    (curr, prev) => {
+      if (curr !== prev && curr !== 0) {
+        cancelAnimation(scale);
+        scale.value = 0;
+        scale.value = withSpring(1, { mass: 0.5 });
+      }
+    },
+  );
+
+  const animatedLeft = useDerivedValue(() => {
+    return withTiming(left.value, {
+      duration: 1000,
+      easing: Easing.inOut(Easing.quad),
+    });
+  }, []);
+
+  const animatedTop = useDerivedValue(() => {
+    return withTiming(top.value, {
+      duration: 1000,
+      easing: Easing.inOut(Easing.quad),
+    });
+  }, []);
+
+  const rMagicCircleStyle = useAnimatedStyle(() => {
+    return {
+      left: animatedLeft.value,
+      top: animatedTop.value,
+    };
+  }, []);
+
+  return (
+    <GestureDetector gesture={tapGesture}>
+      <Animated.View style={styles.container}>
+        <StatusBar style="light" />
+        <Animated.View style={[styles.baseCircle, rStyle]} />
+        <Animated.View style={[styles.baseCircle, rPrevStyle]} />
+        <Animated.View
+          style={[
+            styles.baseCircle,
+            {
+              backgroundColor: '#0074d3',
+            },
+            rMagicCircleStyle,
+          ]}
+        />
+      </Animated.View>
+    </GestureDetector>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#000',
+  },
+  baseCircle: {
+    height: CircleRadius * 2,
+    width: CircleRadius * 2,
+    borderRadius: CircleRadius,
+    backgroundColor: '#2f2f2f',
+    position: 'absolute',
+  },
+});
+
+export { App };
