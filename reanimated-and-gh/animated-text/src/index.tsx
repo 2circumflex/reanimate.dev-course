eaa44af4ac904b80fc00995f6e1a93227bc0151d
Roy <2circumflex@gmail.com>
2024-12-10 22:53:46 +0900

    Animated Text - Skia

--- reanimated-and-gh/animated-text/src/index.tsx
+++ reanimated-and-gh/animated-text/src/index.tsx
@@ -7,7 +7,17 @@ import {
   useSharedValue,
   withTiming,
 } from 'react-native-reanimated';
-import AnimateableText from 'react-native-animateable-text';
+import {
+  Canvas,
+  RadialGradient,
+  Text as SkiaText,
+  SweepGradient,
+  useFont,
+  vec,
+} from '@shopify/react-native-skia';
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const sfProBold = require('../assets/fonts/SF-Pro-Rounded-Bold.otf');
 
 const App = () => {
   const count = useSharedValue(0);
@@ -16,18 +26,44 @@ const App = () => {
     return Math.floor(count.value).toString();
   }, [count]);
 
-  console.log('App rendered', count);
+  const fontSize = 80;
+  const font = useFont(sfProBold, fontSize);
+
+  const canvasWidth = 200;
+  const canvasHeight = 200;
+
+  const x = useDerivedValue(() => {
+    const textWidth = font?.measureText(countString.value).width ?? 0;
+    return canvasWidth / 2 - textWidth / 2;
+  }, [font]);
+
+  const y = useDerivedValue(() => {
+    return canvasHeight / 2 + fontSize / 4;
+  }, [fontSize]);
+
+  const c = useDerivedValue(() => {
+    return vec(x.value, y.value);
+  }, [x, y]);
 
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-      <AnimateableText style={[styles.count]} text={countString} />
+      <Canvas style={{ width: canvasWidth, height: canvasHeight }}>
+        <SkiaText font={font} color={'black'} text={countString} x={x} y={y}>
+          {/* <RadialGradient
+            c={c}
+            r={canvasWidth / 2}
+            colors={['red', 'blue']}
+          /> */}
+          <SweepGradient c={c} colors={['cyan', 'magenta', 'yellow', 'cyan']} />
+        </SkiaText>
+      </Canvas>
       <TouchableOpacity
         onPress={() => {
           count.value = withTiming(Math.random() * 100, { duration: 1000 });
         }}
         style={styles.floatingButton}>
-        <FontAwesome name="random" size={24} color="white" />
+        <FontAwesome name="random" size={24} color="#111" />
       </TouchableOpacity>
     </View>
   );
@@ -36,16 +72,9 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#000',
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  count: {
-    fontSize: 80,
-    fontWeight: 'bold',
-    fontFamily: 'SF-Pro-Rounded-Bold',
-    width: 200,
-    textAlign: 'center',
   },
   floatingButton: {
     position: 'absolute',
@@ -54,7 +83,7 @@ const styles = StyleSheet.create({
     width: 64,
     aspectRatio: 1,
     borderRadius: 32,
-    backgroundColor: '#111',
+    backgroundColor: '#fff',
     justifyContent: 'center',
     alignItems: 'center',
   },
