1e3a2e712dbae8c64b13c75d8921fe9ac8ac3d8d
Roy <2circumflex@gmail.com>
2024-12-13 06:35:18 +0900

    Layout Animations - The Keyframes

--- reanimated-and-gh/layout-animations/src/index.tsx
+++ reanimated-and-gh/layout-animations/src/index.tsx
@@ -1,20 +1,62 @@
 import { StyleSheet, TouchableOpacity } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import { useState } from 'react';
-import Animated, {
-  FadeIn,
-  FadeInDown,
-  FadeOut,
-  FadeOutDown,
-  FlipInEasyX,
-  FlipInXDown,
-  FlipInXUp,
-  FlipOutEasyX,
-  FlipOutXDown,
-  FlipOutXUp,
-  RotateInDownLeft,
-  RotateOutDownLeft,
-} from 'react-native-reanimated';
+import Animated, { Keyframe } from 'react-native-reanimated';
+
+const Perspective = 200;
+
+const InitialKeyframe = {
+  opacity: 0,
+  transform: [
+    {
+      perspective: Perspective,
+    },
+    {
+      translateY: 75,
+    },
+    {
+      rotateX: '-25deg',
+    },
+  ],
+};
+
+const EndKeyframe = {
+  opacity: 1,
+  transform: [
+    {
+      perspective: Perspective,
+    },
+    {
+      translateY: 0,
+    },
+    {
+      rotateX: '0deg',
+    },
+  ],
+};
+
+const CustomFlipIn = new Keyframe({
+  from: InitialKeyframe,
+  to: EndKeyframe,
+}).duration(250);
+
+const CustomFlipOut = new Keyframe({
+  from: EndKeyframe,
+  to: {
+    ...InitialKeyframe,
+    transform: [
+      {
+        perspective: Perspective,
+      },
+      {
+        translateY: -75,
+      },
+      {
+        rotateX: '25deg',
+      },
+    ],
+  },
+}).duration(250);
 
 const App = () => {
   const [isVisible, setIsVisible] = useState(true);
@@ -30,8 +72,8 @@ const App = () => {
       <StatusBar style="auto" />
       {isVisible && (
         <Animated.View
-          entering={FlipInXDown}
-          exiting={FlipOutXDown}
+          entering={CustomFlipIn}
+          exiting={CustomFlipOut}
           style={{
             height: 120,
             aspectRatio: 1,
