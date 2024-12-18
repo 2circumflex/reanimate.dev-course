d66c48b492cd4cdecabd3730305cc01d967a9bf3
Roy <2circumflex@gmail.com>
2024-12-18 23:22:19 +0900

    Shared Transitions - Drag to dismiss gesture

--- reanimated-and-gh/shared-transitions/app/(stack)/_layout.tsx
+++ reanimated-and-gh/shared-transitions/app/(stack)/_layout.tsx
@@ -1,32 +1,35 @@
 import { Stack } from 'expo-router';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
 
 import { Palette } from '../../src/constants';
 
 const Layout = () => {
   return (
-    <Stack
-      screenOptions={{
-        presentation: 'containedTransparentModal',
-        animation: 'fade',
-      }}>
-      <Stack.Screen
-        name="home"
-        options={{
-          headerTitle: 'Home',
-          headerBlurEffect: 'dark',
-          headerTransparent: true,
-          headerTitleStyle: {
-            color: Palette.text,
-          },
-        }}
-      />
-      <Stack.Screen
-        name="detail"
-        options={{
-          headerShown: false,
-        }}
-      />
-    </Stack>
+    <GestureHandlerRootView style={{ flex: 1 }}>
+      <Stack
+        screenOptions={{
+          presentation: 'containedTransparentModal',
+          animation: 'fade',
+        }}>
+        <Stack.Screen
+          name="home"
+          options={{
+            headerTitle: 'Home',
+            headerBlurEffect: 'dark',
+            headerTransparent: true,
+            headerTitleStyle: {
+              color: Palette.text,
+            },
+          }}
+        />
+        <Stack.Screen
+          name="detail"
+          options={{
+            headerShown: false,
+          }}
+        />
+      </Stack>
+    </GestureHandlerRootView>
   );
 };
 
--- reanimated-and-gh/shared-transitions/app/(stack)/detail.tsx
+++ reanimated-and-gh/shared-transitions/app/(stack)/detail.tsx
@@ -1,6 +1,15 @@
 import { BlurView } from 'expo-blur';
 import { useLocalSearchParams, useNavigation } from 'expo-router';
 import { StyleSheet, View } from 'react-native';
+import { Gesture, GestureDetector } from 'react-native-gesture-handler';
+import Animated, {
+  runOnJS,
+  useAnimatedStyle,
+  useDerivedValue,
+  useSharedValue,
+  withSpring,
+} from 'react-native-reanimated';
+import { useCallback } from 'react';
 
 import { AnimatedImage } from '../../src/components/animated-image';
 
@@ -12,11 +21,49 @@ const Detail = () => {
 
   const navigation = useNavigation();
 
+  const changeX = useSharedValue(0);
+  const changeY = useSharedValue(0);
+
+  const goBack = useCallback(() => {
+    navigation.goBack();
+  }, [navigation]);
+
+  const panGesture = Gesture.Pan()
+    .onChange(event => {
+      changeX.value = event.translationX;
+      changeY.value = event.translationY;
+
+      if (Math.abs(event.translationY) > 150) {
+        runOnJS(goBack)();
+      }
+    })
+    .onFinalize(() => {
+      changeX.value = withSpring(0, { mass: 0.5 });
+      changeY.value = withSpring(0, { mass: 0.5 });
+    });
+
+  const scale = useDerivedValue(() => {
+    return Math.max(1 - Math.abs(changeY.value) / 400, 0.5);
+  }, []);
+
+  const rStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        {
+          translateX: changeX.value,
+        },
+        {
+          translateY: changeY.value,
+        },
+        {
+          scale: scale.value,
+        },
+      ],
+    };
+  });
+
   return (
     <View
-      onTouchEnd={() => {
-        navigation.goBack();
-      }}
       style={{
         flex: 1,
         justifyContent: 'center',
@@ -30,22 +77,26 @@ const Detail = () => {
           backgroundColor: 'rgba(0, 0, 0, 0.7)',
         }}
       />
-      <AnimatedImage
-        source={{
-          uri: imageUri,
-        }}
-        sharedTransitionTag={tag}
-        style={{
-          width: '70%',
-          aspectRatio: 1,
-          borderRadius: 25,
-          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-          // @ts-ignore
-          borderCurve: 'continuous',
-        }}
-        contentFit="cover"
-        cachePolicy={'memory-disk'}
-      />
+      <GestureDetector gesture={panGesture}>
+        <Animated.View style={rStyle}>
+          <AnimatedImage
+            source={{
+              uri: imageUri,
+            }}
+            sharedTransitionTag={tag}
+            style={{
+              width: '70%',
+              aspectRatio: 1,
+              borderRadius: 25,
+              // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+              // @ts-ignore
+              borderCurve: 'continuous',
+            }}
+            contentFit="cover"
+            cachePolicy={'memory-disk'}
+          />
+        </Animated.View>
+      </GestureDetector>
     </View>
   );
 };
