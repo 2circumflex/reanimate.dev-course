718f61da01fbf0d842ef43a4a6c8cdb5ebae3d12
Roy <2circumflex@gmail.com>
2024-12-18 22:44:07 +0900

    Shared Transitions - Building the Detail Screen UI

--- reanimated-and-gh/shared-transitions/app/(stack)/detail.tsx
+++ reanimated-and-gh/shared-transitions/app/(stack)/detail.tsx
@@ -1,13 +1,46 @@
-import { View } from 'react-native';
+import { BlurView } from 'expo-blur';
+import { Image } from 'expo-image';
+import { useLocalSearchParams, useNavigation } from 'expo-router';
+import { StyleSheet, View } from 'react-native';
 
 const Detail = () => {
+  const { imageUri, tag } = useLocalSearchParams<{
+    tag: string;
+    imageUri: string;
+  }>();
+
+  const navigation = useNavigation();
+
   return (
     <View
+      onTouchEnd={() => {
+        navigation.goBack();
+      }}
       style={{
         flex: 1,
-        backgroundColor: 'red',
-      }}
-    />
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}>
+      <BlurView
+        intensity={25}
+        tint="systemMaterialDark"
+        style={{
+          ...StyleSheet.absoluteFillObject,
+          backgroundColor: 'rgba(0, 0, 0, 0.7)',
+        }}
+      />
+      <Image
+        source={{
+          uri: imageUri,
+        }}
+        style={{
+          width: '70%',
+          aspectRatio: 1,
+          borderRadius: 25,
+          borderCurve: 'continuous',
+        }}
+      />
+    </View>
   );
 };
 
