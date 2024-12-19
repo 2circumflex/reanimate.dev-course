a7f6cfa955cd57b507c1335c795e86bfd7463ae5
Roy <2circumflex@gmail.com>
2024-12-19 17:42:00 +0900

    Skia Magical Button - Handle the Tap Gesture

--- reanimated-and-gh/skia-magical-button/src/components/magic-button.tsx
+++ reanimated-and-gh/skia-magical-button/src/components/magic-button.tsx
@@ -1,13 +1,12 @@
 import {
   BlurMask,
   Canvas,
-  Circle,
-  Rect,
   RoundedRect,
   SweepGradient,
-  vec,
 } from '@shopify/react-native-skia';
 import React from 'react';
+import { GestureDetector, Gesture } from 'react-native-gesture-handler';
+import Animated, { useSharedValue } from 'react-native-reanimated';
 
 type MagicButtonProps = {
   onPress?: () => void;
@@ -28,34 +27,51 @@ export const MagicButton: React.FC<MagicButtonProps> = ({
   const realY = externalPadding / 2;
   const center = { x: width / 2 + realX, y: height / 2 + realY };
 
+  const isTouched = useSharedValue(false);
+
+  const tapGesture = Gesture.Tap()
+    .maxDuration(10000)
+    .onBegin(() => {
+      isTouched.value = true;
+      console.log('Touched');
+    })
+    .onFinalize(() => {
+      isTouched.value = false;
+      console.log('Finalized');
+    });
+
   return (
-    <Canvas
-      style={{
-        height: realHeight,
-        width: realWidth,
-        backgroundColor: '#000',
-      }}>
-      <RoundedRect
-        x={realX}
-        y={realY}
-        width={width}
-        height={height}
-        color={'red'}
-        r={width / 2}>
-        <SweepGradient
-          c={center}
-          colors={['cyan', 'magenta', 'yellow', 'cyan']}
-        />
-        <BlurMask blur={40} style={'solid'} />
-      </RoundedRect>
-      <RoundedRect
-        x={internalPadding / 2 + realX}
-        y={internalPadding / 2 + realY}
-        width={width - internalPadding}
-        height={height - internalPadding}
-        color={'black'}
-        r={width / 2}
-      />
-    </Canvas>
+    <GestureDetector gesture={tapGesture}>
+      <Animated.View>
+        <Canvas
+          style={{
+            height: realHeight,
+            width: realWidth,
+            backgroundColor: '#000',
+          }}>
+          <RoundedRect
+            x={realX}
+            y={realY}
+            width={width}
+            height={height}
+            color={'red'}
+            r={width / 2}>
+            <SweepGradient
+              c={center}
+              colors={['cyan', 'magenta', 'yellow', 'cyan']}
+            />
+            <BlurMask blur={40} style={'solid'} />
+          </RoundedRect>
+          <RoundedRect
+            x={internalPadding / 2 + realX}
+            y={internalPadding / 2 + realY}
+            width={width - internalPadding}
+            height={height - internalPadding}
+            color={'black'}
+            r={width / 2}
+          />
+        </Canvas>
+      </Animated.View>
+    </GestureDetector>
   );
 };
