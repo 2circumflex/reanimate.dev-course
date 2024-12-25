1765ba8579b157282f9a9d4135ef3ffa3cd0de6f
Roy <2circumflex@gmail.com>
2024-12-25 17:20:38 +0900

    Animate through a path - Optimizing the getPathPoints algorith

--- reanimated-and-gh/animate-through-a-path/src/index.tsx
+++ reanimated-and-gh/animate-through-a-path/src/index.tsx
@@ -1,10 +1,12 @@
+import { useState } from 'react';
 import { StyleSheet, View } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import { Canvas, Circle, Path, SkPath } from '@shopify/react-native-skia';
+import type { SkPath } from '@shopify/react-native-skia';
+import { Canvas, Circle, Path } from '@shopify/react-native-skia';
 import { GestureDetector } from 'react-native-gesture-handler';
 
 import { useDrawGesture } from './hooks/useDrawGesture';
-import { useState } from 'react';
+import { PathGeometry } from './utils/geometry';
 
 type Point = {
   x: number;
@@ -13,9 +15,19 @@ type Point = {
 
 const getPathPoints = (path: SkPath) => {
   const points: Point[] = [];
-  const countPoints = path.countPoints();
-  for (let i = 0; i < countPoints; i++) {
-    const point = path.getPoint(i);
+  // Easy solution
+  // const countPoints = path.countPoints();
+  // for (let i = 0; i < countPoints; i++) {
+  //   const point = path.getPoint(i);
+  //   points.push({ x: point.x, y: point.y });
+  // }
+  // return points;
+
+  // Contour solution
+  const geometry = new PathGeometry(path);
+  const totalLength = geometry.getTotalLength();
+  for (let i = 0; i < totalLength; i++) {
+    const point = geometry.getPointAtLength(i);
     points.push({ x: point.x, y: point.y });
   }
   return points;
@@ -42,9 +54,18 @@ const App = () => {
             strokeWidth={2}
             opacity={pathOpacity}
           />
-          {points.map((point, index) => (
-            <Circle key={index} cx={point.x} cy={point.y} r={2} color="white" />
-          ))}
+          {points.map(
+            (point, index) =>
+              index % 10 === 0 && (
+                <Circle
+                  key={index}
+                  cx={point.x}
+                  cy={point.y}
+                  r={2}
+                  color="red"
+                />
+              ),
+          )}
         </Canvas>
       </GestureDetector>
     </View>
