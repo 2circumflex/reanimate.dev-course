f640b758f37e703a0f7102ea807d9e68cbb1de6a
Roy <2circumflex@gmail.com>
2024-12-26 18:58:22 +0900

    Animated Bezier Curve - Adding the Control Points

--- reanimated-and-gh/animated-bezier-curve/src/index.tsx
+++ reanimated-and-gh/animated-bezier-curve/src/index.tsx
@@ -1,11 +1,55 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
+import { Canvas, Circle } from '@shopify/react-native-skia';
+import { useDerivedValue, useSharedValue } from 'react-native-reanimated';
+
+import { InitialPoints } from './constants';
+
+type Point = {
+  x: number;
+  y: number;
+};
+
+const useSharedControlPoint = (initialPoint: Point) => {
+  const controlPoint = useSharedValue(initialPoint);
+  const cx = useDerivedValue(() => {
+    return controlPoint.value.x;
+  });
+
+  const cy = useDerivedValue(() => {
+    return controlPoint.value.y;
+  });
+
+  return { controlPoint, cx, cy };
+};
 
 const App = () => {
+  const first = useSharedControlPoint(InitialPoints.first);
+  const second = useSharedControlPoint(InitialPoints.second);
+  const third = useSharedControlPoint(InitialPoints.third);
+  const fourth = useSharedControlPoint(InitialPoints.fourth);
+  const controlPoints = [first, second, third, fourth];
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
+      <Canvas
+        style={{
+          flex: 1,
+          backgroundColor: 'black',
+        }}>
+        {controlPoints.map(({ cx, cy }, index) => (
+          <Circle
+            key={index}
+            cx={cx}
+            cy={cy}
+            r={12}
+            color={'white'}
+            strokeWidth={2}
+            style={'stroke'}
+          />
+        ))}
+      </Canvas>
     </View>
   );
 };
@@ -13,9 +57,6 @@ const App = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
 
