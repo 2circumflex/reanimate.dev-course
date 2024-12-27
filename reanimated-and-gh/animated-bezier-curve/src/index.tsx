9380cd946dc9e199b94c0cfacfc9752d19ce5210
Roy <2circumflex@gmail.com>
2024-12-27 13:06:43 +0900

    Animated Bezier Curve - Moving the Control Points

--- reanimated-and-gh/animated-bezier-curve/src/index.tsx
+++ reanimated-and-gh/animated-bezier-curve/src/index.tsx
@@ -1,7 +1,8 @@
 import { StyleSheet, View } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
-import { Canvas, Circle } from '@shopify/react-native-skia';
 import { useDerivedValue, useSharedValue } from 'react-native-reanimated';
+// eslint-disable-next-line import/no-extraneous-dependencies
+import Touchable from 'react-native-skia-gesture';
 
 import { InitialPoints } from './constants';
 
@@ -33,23 +34,32 @@ const App = () => {
   return (
     <View style={styles.container}>
       <StatusBar style="auto" />
-      <Canvas
+      <Touchable.Canvas
         style={{
           flex: 1,
           backgroundColor: 'black',
         }}>
-        {controlPoints.map(({ cx, cy }, index) => (
-          <Circle
-            key={index}
-            cx={cx}
-            cy={cy}
-            r={12}
-            color={'white'}
-            strokeWidth={2}
-            style={'stroke'}
-          />
-        ))}
-      </Canvas>
+        {controlPoints.map(({ cx, cy, controlPoint }, index) => {
+          const onUpdate = (event: { x: number; y: number }) => {
+            'worklet';
+            controlPoint.value = { x: event.x, y: event.y };
+          };
+
+          return (
+            <Touchable.Circle
+              key={index}
+              cx={cx}
+              cy={cy}
+              onStart={onUpdate}
+              onActive={onUpdate}
+              r={12}
+              color={'white'}
+              strokeWidth={2}
+              style={'stroke'}
+            />
+          );
+        })}
+      </Touchable.Canvas>
     </View>
   );
 };
