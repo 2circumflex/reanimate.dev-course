1a1a8e9a4f43d58452fbf13a07dfb3aa05d7e02e
Roy <2circumflex@gmail.com>
2025-01-03 18:44:00 +0900

    Image Shader Transitions - The Skia ImageShaders

--- reanimated-and-gh/image-shader-transitions/src/index.tsx
+++ reanimated-and-gh/image-shader-transitions/src/index.tsx
@@ -1,12 +1,26 @@
 import { StyleSheet, useWindowDimensions, View } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import Slider from '@react-native-community/slider';
-import { Canvas } from '@shopify/react-native-skia';
+import {
+  Canvas,
+  Fill,
+  ImageShader,
+  useImage,
+} from '@shopify/react-native-skia';
+
+const FIRST_IMAGE =
+  'https://images.unsplash.com/photo-1596501048547-e9acb71ca798?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
+
+const SECOND_IMAGE =
+  'https://images.unsplash.com/photo-1531168556467-80aace0d0144?q=80&w=3174&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
 
 const App = () => {
   const { width: windowWidth } = useWindowDimensions();
   const canvasHeight = 600;
   const canvasWidth = windowWidth * 0.95;
+
+  const firstImage = useImage(FIRST_IMAGE);
+  const secondImage = useImage(SECOND_IMAGE);
 
   return (
     <View style={styles.container}>
@@ -23,8 +37,22 @@ const App = () => {
             width: canvasWidth,
           },
           styles.canvas,
-        ]}
-      />
+        ]}>
+        <Fill>
+          <ImageShader
+            image={firstImage}
+            width={canvasHeight}
+            height={canvasHeight}
+            fit={'cover'}
+          />
+          <ImageShader
+            image={secondImage}
+            width={canvasHeight}
+            height={canvasHeight}
+            fit={'cover'}
+          />
+        </Fill>
+      </Canvas>
     </View>
   );
 };
@@ -40,7 +68,6 @@ const styles = StyleSheet.create({
     alignSelf: 'center',
   },
   canvas: {
-    backgroundColor: 'red',
     alignSelf: 'center',
     borderRadius: 25,
     overflow: 'hidden',
