ebecfcbd59b19c6097c45777457f1906850d9b8d
Roy <2circumflex@gmail.com>
2025-01-04 17:39:49 +0900

    Image Shader Transitions - Copying the GL Transitions 🤫

--- reanimated-and-gh/image-shader-transitions/src/index.tsx
+++ reanimated-and-gh/image-shader-transitions/src/index.tsx
@@ -10,7 +10,9 @@ import {
   useImage,
 } from '@shopify/react-native-skia';
 import { useDerivedValue, useSharedValue } from 'react-native-reanimated';
+
 import { glsl } from './helpers/shader-lib';
+import { transition } from './helpers/transition';
 
 const FIRST_IMAGE =
   'https://images.unsplash.com/photo-1596501048547-e9acb71ca798?q=80&w=3540&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
@@ -18,33 +20,40 @@ const FIRST_IMAGE =
 const SECOND_IMAGE =
   'https://images.unsplash.com/photo-1531168556467-80aace0d0144?q=80&w=3174&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D';
 
-const shader = glsl`
-uniform shader image1;
-uniform shader image2;
+const butterFlyWaveShader = glsl`
+// Author: mandubian
+// License: MIT
+float amplitude = 2.0;
+float waves = 5.0;
+float colorSeparation = 0.6;
 
-uniform float progress;
-uniform float2 resolution;
-
-// uv(0,0) - (1,1)
-// fragCoord (0,0) - (width, height)
-half4 getFromColor(float2 uv, float2 resolution) {
-  return image1.eval(uv * resolution);
+float PI = 3.14159265358979323846264;
+float compute(vec2 p, float progress, vec2 center) {
+vec2 o = p*sin(progress * amplitude)-center;
+// horizontal vector
+vec2 h = vec2(1., 0.);
+// butterfly polar function (don't ask me why this one :))
+float theta = acos(dot(o, h)) * waves;
+return (exp(cos(theta)) - 2.*cos(4.*theta) + pow(sin((2.*theta - PI) / 24.), 5.)) / 10.;
+}
+vec4 transition(vec2 uv) {
+  vec2 p = uv.xy / vec2(1.0).xy;
+  float inv = 1. - progress;
+  vec2 dir = p - vec2(.5);
+  float dist = length(dir);
+  float disp = compute(p, progress, vec2(0.5, 0.5)) ;
+  vec4 texTo = getToColor(p + inv*disp);
+  vec4 texFrom = vec4(
+  getFromColor(p + progress*disp*(1.0 - colorSeparation)).r,
+  getFromColor(p + progress*disp).g,
+  getFromColor(p + progress*disp*(1.0 + colorSeparation)).b,
+  1.0);
+  return texTo*progress + texFrom*inv;
 }
 
-half4 getToColor(float2 uv, float2 resolution) {
-  return image2.eval(uv * resolution);
-}
-
-half4 main(float2 xy) {
-  return mix(
-    getFromColor(xy / resolution, resolution),
-    getToColor(xy / resolution, resolution),
-    progress
-  );
-}
 `;
 
-const shaderRuntimeEffect = Skia.RuntimeEffect.Make(shader);
+const butterFlyShaderEffect = transition(butterFlyWaveShader);
 
 const App = () => {
   const { width: windowWidth } = useWindowDimensions();
@@ -80,7 +89,7 @@ const App = () => {
           styles.canvas,
         ]}>
         <Fill>
-          <Shader source={shaderRuntimeEffect!} uniforms={uniforms}>
+          <Shader source={butterFlyShaderEffect!} uniforms={uniforms}>
             <ImageShader
               image={firstImage}
               width={canvasHeight}
