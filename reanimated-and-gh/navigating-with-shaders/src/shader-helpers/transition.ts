33ffb53271ca1bf4a9f17d5624a192f5c5586b9e
Roy <2circumflex@gmail.com>
2025-01-05 17:42:32 +0900

    Navigating with Shaders - Boilerplate explanation

---
+++ reanimated-and-gh/navigating-with-shaders/src/shader-helpers/transition.ts
@@ -0,0 +1,30 @@
+import { frag } from './shader-lib';
+
+export type Transition = string;
+
+// You can find it in the RN Skia
+// examples repo: https://raw.githubusercontent.com/Shopify/react-native-skia/main/example/src/Examples/Transitions/transitions/Base.ts
+export const transition = (openGLTransition: Transition) => {
+  return frag`
+  uniform shader image1;
+  uniform shader image2;
+
+  uniform float progress;
+  uniform float2 resolution;
+  
+  half4 getFromColor(float2 uv) {
+    return image1.eval(uv * resolution);
+  }
+  
+  half4 getToColor(float2 uv) {
+    return image2.eval(uv * resolution);
+  }
+  
+  ${openGLTransition}
+
+  half4 main(vec2 xy) {
+    vec2 uv = xy / resolution;
+    return transition(uv);
+  }
+  `;
+};
