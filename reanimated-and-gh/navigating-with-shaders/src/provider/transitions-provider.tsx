75cf7546b2ffd9df6cda938967588c82c821d294
Roy <2circumflex@gmail.com>
2025-01-06 00:34:38 +0900

    Navigating with Shaders - The makeImageFromSnapshot

--- reanimated-and-gh/navigating-with-shaders/src/provider/transitions-provider.tsx
+++ reanimated-and-gh/navigating-with-shaders/src/provider/transitions-provider.tsx
@@ -1,5 +1,13 @@
-import { createContext, useCallback, useContext } from 'react';
-import { View } from 'react-native';
+import type { SkImage } from '@shopify/react-native-skia';
+import {
+  Canvas,
+  Fill,
+  ImageShader,
+  makeImageFromView,
+} from '@shopify/react-native-skia';
+import { createContext, useCallback, useContext, useRef } from 'react';
+import { useWindowDimensions, View } from 'react-native';
+import { useSharedValue } from 'react-native-reanimated';
 
 const TransitionsContext = createContext({
   prepareTransition: () => {},
@@ -16,16 +24,46 @@ type TransitionsProviderProps = {
 export const TransitionsProvider: React.FC<TransitionsProviderProps> = ({
   children,
 }) => {
-  const prepareTransition = useCallback(() => {
+  const viewRef = useRef<View>(null);
+  const firstImage = useSharedValue<SkImage | null>(null);
+
+  const prepareTransition = useCallback(async () => {
+    const imageSnapshot = await makeImageFromView(viewRef);
+    firstImage.value = imageSnapshot;
     console.log('prepareTransition');
-  }, []);
+  }, [firstImage]);
+
+  const { width: windowWidth, height: windowHeight } = useWindowDimensions();
 
   return (
     <TransitionsContext.Provider
       value={{
         prepareTransition,
       }}>
-      <View style={{ flex: 1 }}>{children}</View>
+      <Canvas
+        style={{
+          position: 'absolute',
+          top: 0,
+          left: 0,
+          right: 0,
+          bottom: 0,
+          zIndex: 1000,
+          backgroundColor: 'red',
+          opacity: 0.5,
+          pointerEvents: 'none',
+        }}>
+        <Fill>
+          <ImageShader
+            image={firstImage}
+            width={windowWidth}
+            height={windowHeight}
+            fit="cover"
+          />
+        </Fill>
+      </Canvas>
+      <View ref={viewRef} style={{ flex: 1 }}>
+        {children}
+      </View>
     </TransitionsContext.Provider>
   );
 };
