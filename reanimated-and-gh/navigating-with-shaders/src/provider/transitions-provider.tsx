703d483c81df239c6c40918a4276d5fc570315a6
Roy <2circumflex@gmail.com>
2025-01-07 10:45:53 +0900

    Navigating with Shaders - The Shader Transition

--- reanimated-and-gh/navigating-with-shaders/src/provider/transitions-provider.tsx
+++ reanimated-and-gh/navigating-with-shaders/src/provider/transitions-provider.tsx
@@ -4,10 +4,19 @@ import {
   Fill,
   ImageShader,
   makeImageFromView,
+  Shader,
 } from '@shopify/react-native-skia';
 import { createContext, useCallback, useContext, useRef } from 'react';
 import { useWindowDimensions, View } from 'react-native';
-import { useSharedValue } from 'react-native-reanimated';
+import Animated, {
+  useAnimatedStyle,
+  useDerivedValue,
+  useSharedValue,
+  withTiming,
+} from 'react-native-reanimated';
+
+import { glsl } from '../shader-helpers/shader-lib';
+import { transition } from '../shader-helpers/transition';
 
 const TransitionsContext = createContext({
   prepareTransition: (): Promise<void> => {
@@ -26,6 +35,28 @@ type TransitionsProviderProps = {
   children: React.ReactNode;
 };
 
+const shader = glsl`
+// Author: pschroen
+// License: MIT
+
+vec2 direction = vec2(-1.0, 1.0);
+
+const float smoothness = 0.5;
+const vec2 center = vec2(0.5, 0.5);
+
+vec4 transition (vec2 uv) {
+  vec2 v = normalize(direction);
+  v /= abs(v.x) + abs(v.y);
+  float d = v.x * center.x + v.y * center.y;
+  float m = 1.0 - smoothstep(-smoothness, 0.0, v.x * uv.x + v.y * uv.y - (d - 0.5 + progress * (1.0 + smoothness)));
+  return mix(getFromColor((uv - 0.5) * (1.0 - m) + 0.5), getToColor((uv - 0.5) * m + 0.5), m);
+}
+`;
+
+const ShaderTransitionEffect = transition(shader);
+
+const AnimatedCanvas = Animated.createAnimatedComponent(Canvas);
+
 export const TransitionsProvider: React.FC<TransitionsProviderProps> = ({
   children,
 }) => {
@@ -33,19 +64,45 @@ export const TransitionsProvider: React.FC<TransitionsProviderProps> = ({
   const firstImage = useSharedValue<SkImage | null>(null);
   const secondImage = useSharedValue<SkImage | null>(null);
 
+  const progress = useSharedValue(0);
+
   const prepareTransition = useCallback(async () => {
     const imageSnapshot = await makeImageFromView(viewRef);
     firstImage.value = imageSnapshot;
-    console.log('prepareTransition');
   }, [firstImage]);
 
   const runTransition = useCallback(async () => {
     const imageSnapshot = await makeImageFromView(viewRef);
     secondImage.value = imageSnapshot;
-    console.log('runTransition');
-  }, [secondImage]);
+    progress.value = withTiming(
+      1,
+      {
+        duration: 2000,
+      },
+      isFinished => {
+        if (isFinished) {
+          progress.value = 0;
+          firstImage.value = null;
+          secondImage.value = null;
+        }
+      },
+    );
+  }, [firstImage, secondImage, progress]);
 
   const { width: windowWidth, height: windowHeight } = useWindowDimensions();
+
+  const uniforms = useDerivedValue(() => {
+    return {
+      resolution: [windowWidth, windowHeight],
+      progress: progress.value,
+    };
+  }, [windowWidth, windowHeight]);
+
+  const rCanvasStyle = useAnimatedStyle(() => {
+    return {
+      opacity: progress.value > 0 ? 1 : 0,
+    };
+  }, []);
 
   return (
     <TransitionsContext.Provider
@@ -53,33 +110,37 @@ export const TransitionsProvider: React.FC<TransitionsProviderProps> = ({
         prepareTransition,
         runTransition,
       }}>
-      <Canvas
-        style={{
-          position: 'absolute',
-          top: 0,
-          left: 0,
-          right: 0,
-          bottom: 0,
-          zIndex: 1000,
-          backgroundColor: 'red',
-          opacity: 1,
-          pointerEvents: 'none',
-        }}>
+      <AnimatedCanvas
+        style={[
+          {
+            position: 'absolute',
+            top: 0,
+            left: 0,
+            right: 0,
+            bottom: 0,
+            zIndex: 1000,
+            backgroundColor: 'red',
+            pointerEvents: 'none',
+          },
+          rCanvasStyle,
+        ]}>
         <Fill>
-          <ImageShader
-            image={firstImage}
-            width={windowWidth}
-            height={windowHeight}
-            fit="cover"
-          />
-          <ImageShader
-            image={secondImage}
-            width={windowWidth}
-            height={windowHeight}
-            fit="cover"
-          />
+          <Shader source={ShaderTransitionEffect} uniforms={uniforms}>
+            <ImageShader
+              image={secondImage}
+              width={windowWidth}
+              height={windowHeight}
+              fit="cover"
+            />
+            <ImageShader
+              image={firstImage}
+              width={windowWidth}
+              height={windowHeight}
+              fit="cover"
+            />
+          </Shader>
         </Fill>
-      </Canvas>
+      </AnimatedCanvas>
       <View ref={viewRef} style={{ flex: 1 }}>
         {children}
       </View>
